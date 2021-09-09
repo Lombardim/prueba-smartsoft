7fab0ebddb0220c4299b34eb37cb790fc6020045
Lombardim <lombardim@uninorte.edu.co>
2021-09-09 02:01:46 +0200

    login html and resposive added
    Fully responsive
    HTML finished
    Functionality still needs to be added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,17 @@ export class LoginComponent implements OnInit {
   vPosition: MatSnackBarVerticalPosition  = "bottom";
 
 
-  userName = new FormControl('', [Validators.required]);
+  userName = new FormControl('', Validators.required);
+  passWord = new FormControl('', [Validators.required, Validators.minLength(6)]);
   hide: boolean = true;
   
-  constructor(private _snackBar: MatSnackBar) { }
+  constructor(private _snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  openRegister(): void {
+    this.router.navigate(['register']);
   }
 
   openSnackBar(text: string, close: string) {
@@ -28,11 +34,22 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  getErrorMessage() {
+  getErrorMessageU() {
     if (this.userName.hasError('required')) {
-      return 'Debe escribir un nombre de usuario';
+      return 'Debe escribir su nombre de usuario';
     }
     return this.userName.getError("...");
+  }
+
+  getErrorMessageP() {
+    if (this.passWord.hasError('required')){
+      return 'Debe escribir su contraseña';
+    }
+    if (this.passWord.hasError('minlength')){
+      return 'La longitud mínima debe ser de 6 caracteres';
+    }
+
+    return '';
   }
 
 }
